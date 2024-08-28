7f588e579c4dd585e2381783ff1e02b8bbc5465e
Micheil <fouriemicheil1@gmail.com>
2024-08-28 08:29:35 +0200

    Dependencies update

--- src/LineItem.js
+++ src/LineItem.js
@@ -5,7 +5,8 @@ function LineItem({ item, handleCheck, handleDelete }) {
         <li key={item.id} onDoubleClick={() => { handleCheck(item.id) }}>
             <input onChange={() => { handleCheck(item.id) }} type="checkbox" checked={item.checked} />
             <label>{item.item}</label>
-            <i className="fa-trash"><FaTrashAlt onClick={() => { handleDelete(item.id) }} role="button" tabIndex="0" aria-label={`Delete ${item.item}`} /></i>
+            <i className="fa-trash"><FaTrashAlt onClick={() => { handleDelete(item.id) }}
+                role="button" tabIndex="0" aria-label={`Delete ${item.item}`} /></i>
         </li>
     )
 }
